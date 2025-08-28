50414d21588a90acba2beeb2684bb3c2708e8e3e
Cuzeac Florin <mail@cuzeacflorin.fr>
2025-08-28 16:33:44 +0200

    update taze.config.ts

--- taze.config.ts
+++ taze.config.ts
@@ -1,24 +1,33 @@
-import { defineConfig } from 'taze';
+import { type CheckOptions, defineConfig } from 'taze';
 
-export default defineConfig({
+const config: Partial<CheckOptions> = defineConfig({
 	// ignore packages from bumping
 	exclude: ['react-grid-layout'],
+
 	// fetch latest package info from registry without cache
 	force: true,
+
 	// write to package.json
 	write: true,
+
 	// run `npm install` or `yarn install` right after bumping
 	install: false,
-	// ignore paths for looking for package.json in monorepo
-	ignorePaths: ['**/node_modules/**', '**/test/**'],
+
 	// ignore package.json that in other workspaces (with their own .git,pnpm-workspace.yaml,etc.)
 	ignoreOtherWorkspaces: true,
+
+	// ignore paths for looking for package.json in monorepo
+	ignorePaths: ['**/node_modules/**', '**/test/**'],
+
 	// override with different bumping mode for each package
 	packageMode: {
 		typescript: 'major',
 	},
+
 	// disable checking for "overrides" package.json field
 	depFields: {
 		overrides: false,
 	},
 });
+
+export default config;
